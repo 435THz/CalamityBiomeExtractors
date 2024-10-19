443f548752b4feb361f0bb2ca17f838a2dab7596
MistressNebula <delbovemarie@gmail.com>
2024-10-19 16:52:08 +0200

    Create CalamityExtractionTiers.cs

---
+++ Common/CalamityExtractionTiers.cs
@@ -0,0 +1,9 @@
+﻿namespace CalamityBiomeExtractors.Common
+{
+    internal abstract class CalamityExtractionTiers
+    {
+        public static readonly int SPECTRAL = 7000;
+        public static readonly int AURIC = 8000;
+        public static readonly int EXO = 9000;
+    }
+}
