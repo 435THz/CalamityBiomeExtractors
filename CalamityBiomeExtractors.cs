e9aa0040768cff08e4e737af34e82597d41610d4
MistressNebula <delbovemarie@gmail.com>
2024-10-24 16:00:09 +0200

    Update CalamityBiomeExtractors.cs

--- CalamityBiomeExtractors.cs
+++ CalamityBiomeExtractors.cs
@@ -5,7 +5,9 @@ namespace CalamityBiomeExtractors
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class CalamityBiomeExtractors : Mod
     {
-        private static readonly object LocBase = "Mods.CalamityBiomeExtractors";
+        public static CalamityBiomeExtractors Instance => ModContent.GetInstance<CalamityBiomeExtractors>();
+        
+        private static readonly string LocBase = "Mods.CalamityBiomeExtractors";
 
         private static string LocItemsCategory => $"{LocBase}.Items";
         private static string LocItemsExtractor(string variant) => $"{LocItemsCategory}.{variant}ExtractorItem";
@@ -14,7 +16,8 @@ namespace CalamityBiomeExtractors
 
         private static string LocTextCategory => $"{LocBase}.Text";
         public static string LocPoolNames => $"{LocTextCategory}.PoolNames";
-        public static object LocArticles => $"{LocTextCategory}.Articles";
+        public static string LocArticles => $"{LocTextCategory}.Articles";
+        public static string LocTiers => $"{LocTextCategory}.Tiers";
 
     }
 }
